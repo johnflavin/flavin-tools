510477c1baf5dfdb854e48a154d55a0f2ba84ff1
John Flavin <flavinj@gmail.com>
2021-06-13 13:22:21 -0500

    Remove old cnda and xnat scripts

--- fs-relaunch-bulk-relaunch/launch.batch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-N=$1
-
-echo "source batch${N}.sourceme | tee -a batch${N}.log" | tee batch${N}.log
-source batch${N}.sourceme | tee -a batch${N}.log
--- refresh-minion-tokens.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-USERTOKENFILE=$1
-UTOKEN=`sed 's/ .*//' < ~/flavin-fs/DIANDF/$USERTOKENFILE`
-PTOKEN=`sed 's/.* //' < ~/flavin-fs/DIANDF/$USERTOKENFILE`
-NEWTOKENSTR=`curl -ks -u $UTOKEN:$PTOKEN https://cnda.wustl.edu/data/services/tokens/issue/$UTOKEN/$PTOKEN`
-
-# echo `sed -e 's/{"alias":"//' -e 's/","secret":"/ /' -e 's/"}//' <<< $NEWTOKENSTR` > ~/flavin-fs/DIANDF/$USERTOKENFILE
-echo -n `sed -e 's/{"alias":"//' -e 's/","secret":"/ /' -e 's/"}//' <<< $NEWTOKENSTR`

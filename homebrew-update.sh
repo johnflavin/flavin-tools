6112a0a094162324eda0d1839fb68363ce1c894c
John Flavin <johnflavin@flywheel.io>
2022-06-08 08:49:26 -0500

    Remove --greedy from homebrew cask update

--- homebrew-update.sh
+++ homebrew-update.sh
@@ -4,4 +4,4 @@ ping -o github.com > /dev/null || exit 1
 
 /opt/homebrew/bin/brew update && \
 /opt/homebrew/bin/brew upgrade && \
-/opt/homebrew/bin/brew upgrade --cask --greedy
+/opt/homebrew/bin/brew upgrade --cask

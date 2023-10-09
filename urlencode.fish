2e7f83fcc762203620725eea8972578064029801
John Flavin <johnflavin@flywheel.io>
2023-10-09 10:56:33 -0500

    Use python3 for url[en|de]code

--- urlencode.fish
+++ urlencode.fish
@@ -1,3 +1,3 @@
 function urlencode -d "URL encode string"
-    python -c "import sys, urllib.parse as ul; input = sys.argv[1:] if sys.argv[1:] else (l.strip() for l in sys.stdin.readlines()); result=(ul.quote_plus(s) for s in input); print(' '.join(result))" $argv
+    python3 -c "import sys, urllib.parse as ul; input = sys.argv[1:] if sys.argv[1:] else (l.strip() for l in sys.stdin.readlines()); result=(ul.quote_plus(s) for s in input); print(' '.join(result))" $argv
 end
